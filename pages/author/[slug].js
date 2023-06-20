fcdaa62cb0e1c91e5cab1ce4c3ef7b06493e00fd
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2023-06-20 11:48:46 +0200

    author page

---
+++ pages/author/[slug].js
@@ -0,0 +1,86 @@
+import { gql, useQuery } from '@apollo/client';
+import React, {useEffect, useState} from 'react';
+import { PostsGrid } from '../../components';
+import { useRouter } from 'next/router';
+
+const RELATED = gql`
+  query GetRelated (
+    $slug: String!
+  ) {
+    posts(where: {authorName: $slug}, first: 25)  {
+      edges {
+        node {
+          id
+          databaseId
+          title
+          slug
+          date
+          featuredImage{
+            node{
+              mediaItemUrl
+            }
+          }
+          categories{
+            nodes{
+              name
+            }
+          }
+          tags{
+            nodes{
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const Page = () => {
+
+  const router = useRouter();
+  const slug = router.query.slug || [];
+
+  const { data } = useQuery(RELATED, {
+    variables: {slug},
+  });
+
+  const [author, setAuthor] = useState([]);
+
+  useEffect(() => {
+    async function fetchAuthors() {
+      const response = await fetch(`https://apriatst.artez.nl/wp-json/wp/v2/ppma_author?slug=${slug}`);
+      const jsonData = await response.json();
+      setAuthor(jsonData[0])
+    }
+    fetchAuthors()
+  }, [])
+
+
+  return (
+    <main className="article">
+      <div className="wrap">
+        <div className='info-bar'>
+          <div className='date'>
+            <div className='field'>COUNT</div>
+            <div className='data'>{author.count}</div>
+          </div>
+          <div className='date'>
+            <div className='field'>ROLE</div>
+            <div className='data'>{author.taxonomy}</div>
+          </div>
+        </div>
+        <h1 className='headline'>{author.name}</h1>
+        <div className='intro' dangerouslySetInnerHTML={{ __html: author.acf?.bibliography ?? '' }} />
+        {data &&
+          <PostsGrid
+            posts={data.posts.edges}
+          />
+        }
+      </div>
+    </main>
+  )
+}
+
+
+export default Page
