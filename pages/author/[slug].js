3876de511e5d0056098ca51183cae57f172ec6f7
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2023-06-20 12:12:57 +0200

    author page

--- pages/author/[slug].js
+++ pages/author/[slug].js
@@ -1,12 +1,30 @@
 import { gql, useQuery } from '@apollo/client';
 import React, {useEffect, useState} from 'react';
-import { PostsGrid } from '../../components';
+import { BlogInfoFragment } from '../../fragments/GeneralSettings';
+import {
+  Header,
+  PostsGrid,
+  SEO,
+} from '../../components';
 import { useRouter } from 'next/router';
 
 const RELATED = gql`
+  ${BlogInfoFragment}
   query GetRelated (
     $slug: String!
   ) {
+    menu(id: "dGVybToxMQ==") {
+      menuItems {
+        nodes {
+          label
+          url
+          uri
+        }
+      }
+    }
+    generalSettings {
+      ...BlogInfoFragment
+    }
     posts(where: {authorName: $slug}, first: 25)  {
       edges {
         node {
@@ -54,31 +72,44 @@ const Page = () => {
       setAuthor(jsonData[0])
     }
     fetchAuthors()
+    
   }, [])
 
-
   return (
-    <main className="article">
-      <div className="wrap">
-        <div className='info-bar'>
-          <div className='date'>
-            <div className='field'>COUNT</div>
-            <div className='data'>{author.count}</div>
+    <>
+    {data &&
+      <>
+        <SEO
+          title={data?.generalSettings.title}
+          description={data?.generalSettings.description}
+        />
+        <Header
+          title={data?.generalSettings.title}
+          description={data?.generalSettings.description}
+          menuItems={data?.menu?.menuItems?.nodes}
+        />
+        <main className="article">
+          <div className="wrap">
+            <div className='info-bar'>
+              <div className='date'>
+                <div className='field'>COUNT</div>
+                <div className='data'>{author.count}</div>
+              </div>
+              <div className='date'>
+                <div className='field'>ROLE</div>
+                <div className='data'>{author.taxonomy}</div>
+              </div>
+            </div>
+            <h1 className='headline'>{author.name}</h1>
+            <div className='intro' dangerouslySetInnerHTML={{ __html: author.acf?.bibliography ?? '' }} />
+              <PostsGrid
+                posts={data.posts.edges}
+              />
           </div>
-          <div className='date'>
-            <div className='field'>ROLE</div>
-            <div className='data'>{author.taxonomy}</div>
-          </div>
-        </div>
-        <h1 className='headline'>{author.name}</h1>
-        <div className='intro' dangerouslySetInnerHTML={{ __html: author.acf?.bibliography ?? '' }} />
-        {data &&
-          <PostsGrid
-            posts={data.posts.edges}
-          />
-        }
-      </div>
-    </main>
+        </main>
+      </>
+    }
+    </>
   )
 }
 
