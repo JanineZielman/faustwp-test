2e52f5bc9199694ba12bc25a02dd59f374141f53
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2023-08-08 15:48:58 +0200

    index link fix

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -22,6 +22,7 @@ export default function Header({
             <a href="/" className="logo">
               <svg className="apria_logo" width="100%" height="100%" viewBox="0 0 100 100"><circle cx="50%" cy="50%" r="50"></circle></svg>
             </a>
+            <a href="/" className="logo-link"></a>
           </div>
           <div
             type="button"
