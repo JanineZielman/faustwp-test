368ca78df15106799f4d7d3de6cef24a5b4bbbd7
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2023-10-17 12:24:35 +0200

    custom script 2

--- public/script.js
+++ public/script.js
@@ -16,3 +16,24 @@ function showOverlay() {
 
     player.play();
 };
+
+function hideOverlay2 () {  
+    const player = document.getElementById('earthbnb');
+    const container = document.getElementById('videoContainer');
+
+    player.pause();
+    container.classList.add('is-hidden');
+    document.documentElement.style.overflow = '';
+    document.documentElement.style.height = '';
+};
+
+function showOverlay2 () {
+const player = document.getElementById('earthbnb');
+const container = document.getElementById('videoContainer');
+
+container.classList.remove('is-hidden');
+document.documentElement.style.overflow = 'hidden';
+document.documentElement.style.height = '100%';
+
+player.play();
+};
